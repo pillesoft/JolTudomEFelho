e6181397f862dec1a40f2b73d4860043e4950fed
pillesoft <pillesoft@gmail.com>
2014-12-07 16:40:44 +0100

    ping es pingdb mukodik publish beallitas is jonak tunik

---
+++ JolTudomE_Api/Controllers/ACTController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JolTudomE_Api.Controllers {
+  // this is a controller to test if the server is ready - AccessTest
+  [RoutePrefix("api/act")]
+  public class ACTController : BaseController {
+    [Route("ping")]
+    [AllowAnonymous]
+    [HttpGet]
+    public string ping() {
+      return "pong";
+    }
+
+    [Route("pingdb")]
+    [AllowAnonymous]
+    [HttpGet]
+    public string pingdb() {
+      int howmany = DBContext.Database.SqlQuery<int>("select count(*) from users.person").Single();
+      return string.Format("{0} sor van a tablaba", howmany);
+    }
+
+  }
+}
